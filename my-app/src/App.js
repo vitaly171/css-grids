44cc78a7619ee4ec0bae7721c10f9e81b399b800
vitaly171 <72026466+vitaly171@users.noreply.github.com>
2023-03-19 23:07:16 +0300

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,21 +4,51 @@ function App() {
   return (
     <>
       <section>
-        Grid
-        <ul>
+        <ul className="grid">
+          {" "}
+          Grid
           <li>1</li>
           <li>2</li>
           <li>3</li>
           <li>4</li>
         </ul>
       </section>
-      <section className="inlineGrid">
-        Inline Grid
-        <ul>
+      <section>
+        <ul className="inlineGrid">
+          {" "}
+          Inline Grid
           <li>5</li>
           <li>6</li>
           <li>7</li>
           <li>8</li>
+        </ul>
+      </section>
+      <section>
+        <ul className="columnsGrid">
+          <li>9</li>
+          <li>10</li>
+          <li>11</li>
+          <li>12</li>
+          <li>13</li>
+          <li>14</li>
+        </ul>
+      </section>
+      <section>
+        <ul className="rowsGrid">
+          <li>15</li>
+          <li>16</li>
+          <li>17</li>
+          <li>18</li>
+          <li>19</li>
+          <li>20</li>
+        </ul>
+      </section>
+      <section>
+        <ul className="stretch">
+          <li>21</li>
+          <li>22</li>
+          <li>23</li>
+          <li>24</li>
         </ul>
       </section>
     </>
